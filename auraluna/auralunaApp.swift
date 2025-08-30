9e5fb5afc863d68ad7de946e475c41d079ab3444
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-08-30 19:17:18 -0300

    project init

---
+++ auraluna/auralunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  auralunaApp.swift
+//  auraluna
+//
+//  Created by Adrian Rodriguez on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct auralunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
